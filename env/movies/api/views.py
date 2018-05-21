75c380d35b5ac5acc0743034586dab0e52f2013e
keith bonarrigo <keith@keithbonline.com>
2018-05-20 18:47:56 -0700

    working api

---
+++ env/movies/api/views.py
@@ -0,0 +1,14 @@
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+
+from .models import Movie
+from .serializers import MovieSerializer
+
+
+class MovieCreateView(ListCreateAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+class MovieDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
